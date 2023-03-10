8e4fee38a4a01b64ad1b11da3f1a2f587a325fff
Alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-03-10 05:21:37 -0300

    ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,25 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	
+	private char column;
+	private int row;
+	
+	
+	public ChessPosition(char column, int row) {
+		this.column = column;
+		this.row = row;
+	}
+	
+	
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	
+	protected ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)(position.getColumn() + 'a'), 8 - position.getRow());
+	}
+}
