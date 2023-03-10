572bf737e2708841018ca637d096442d4bbd932d
Alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-03-10 04:52:09 -0300

    Classes e metodos do pacote boardgame

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,34 @@
+package boardgame;
+
+public abstract class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+	
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
